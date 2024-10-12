b2fa25a8911221b1e9968302dd26d909ed9c8ee9
nelsonoftrafalgar <nelsonoftrafalgar@gmail.com>
2024-10-12 19:32:04 +0200

    config vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
+import { defineConfig as defineVitestConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+const viteConfig = defineConfig({
+	plugins: [react()]
 })
+
+const vitestConfig = defineVitestConfig({
+	test: {
+		environment: 'jsdom'
+	}
+})
+
+export default { ...viteConfig, ...vitestConfig }
